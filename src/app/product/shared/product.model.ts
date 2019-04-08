7b99014cdc9d8e2f2544435d7426588869aa0f16
NguyenNam <namnguyen161295@gmai.com>
2019-04-09 01:54:25 +0700

    Sprint 1

---
+++ src/app/product/shared/product.model.ts
@@ -0,0 +1,21 @@
+import { ShoppingCart } from '../../shoppingCart/shared/shoppingCart.model';
+
+export class Product{
+
+	static readonly CATEGORIES = ["Arts", "Automotive", "Books", "Computers",
+								  "Digital Music", "Electronics","Kindle Store", "Prime Video", 
+								  "Fashion", "Household", "Scientific", 
+								  "Movie", "CD", "Software", "Video Games", "Sports"];
+
+	_id: string;
+	name: string;
+	street: string;
+	category: string;
+	image: string;
+	bedrooms: number;
+	description: string;
+	price: number;
+	shared: boolean;
+	createAt: string;
+	shoppingCarts: ShoppingCart[];
+}
